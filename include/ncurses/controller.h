3f498ffd755637ae80740f9d757007b9d30da858
Yaël Mused <vikuscode@2a01cb0c06b634002d4f3de20b58b3db.ipv6.abo.wanadoo.fr>
2025-12-29 16:54:02 +0100

    Modification des include pour bien séparer ncurses et SDL

---
+++ include/ncurses/controller.h
@@ -0,0 +1,6 @@
+#pragma once
+
+/*
+    La vue doit traduire ce qu'on va créer dans le modèle, le controleur doit mettre à jour le modèle 
+    Donc en gros, le modèle dépend des controles qui dépendent de la vue
+*/
