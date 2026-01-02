0a52b4ae0a958c065a28657a76159ac13ef73d5f
Yaël Mused <vikuscode@2a01cb0c06b634002d4f3de20b58b3db.ipv6.abo.wanadoo.fr>
2026-01-02 19:25:12 +0100

    Modification des fichiers correspondant à ncurses

--- include/view_ncurses.h
+++ include/view_ncurses.h
@@ -1,10 +1,64 @@
+/**
+ * @file view_ncurses.h
+ * @brief Vue ncurses pour Space Invaders
+ */
+
 #ifndef VIEW_NCURSES_H
 #define VIEW_NCURSES_H
+
 #include "model.h"
 
-void init_ncurses_view();
-void draw_ncurses_view(GameState *game);
-void close_ncurses_view();
-int get_ncurses_input();
+/* ============================================================================
+ * CONSTANTES DE COULEURS
+ * ============================================================================ */
 
-#endif
+#define COLOR_PLAYER        1
+#define COLOR_ENEMY         2
+#define COLOR_BULLET        3
+#define COLOR_ENEMY_BULLET  4
+#define COLOR_SHIELD        5
+#define COLOR_HUD           6
+#define COLOR_BORDER        7
+#define COLOR_TITLE         8
+#define COLOR_MENU          9
+#define COLOR_GAME_OVER     10
+#define COLOR_WIN           11
+#define COLOR_PAUSE         12
+
+/* ============================================================================
+ * CONSTANTES DE CARACTÈRES
+ * ============================================================================ */
+
+#define PLAYER_CHAR         'A'
+#define BULLET_CHAR         '|'
+#define ENEMY_BULLET_CHAR   '!'
+#define SHIELD_CHAR         '#'
+
+/* ============================================================================
+ * FONCTIONS PUBLIQUES
+ * ============================================================================ */
+
+/**
+ * @brief Initialise ncurses
+ */
+void init_ncurses(void);
+
+/**
+ * @brief Nettoie et ferme ncurses
+ */
+void cleanup_ncurses(void);
+
+/**
+ * @brief Affiche l'état du jeu selon la vue actuelle
+ * @param game Pointeur vers l'état du jeu
+ */
+void render_ncurses(GameState *game);
+
+/**
+ * @brief Gère les entrées clavier
+ * @param game Pointeur vers l'état du jeu
+ * @return 1 pour continuer, 0 pour quitter
+ */
+int handle_input_ncurses(GameState *game);
+
+#endif /* VIEW_NCURSES_H */
